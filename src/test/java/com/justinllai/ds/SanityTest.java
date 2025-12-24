f599eddc0a338e0df0735ec88d78fe57c2d9c32a
justinllai <justin29lai@gmail.com>
2025-12-23 16:51:40 -0800

    Set up Maven project and JUnit test pipeline

---
+++ src/test/java/com/justinllai/ds/SanityTest.java
@@ -0,0 +1,12 @@
+package com.justinllai.ds;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SanityTest {
+
+    @Test
+    void junitWorks() {
+        assertEquals(2, 1 + 1);
+    }
+}
